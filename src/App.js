bac65c038f81f1a1773cc85c65bea8058f365e41
pratik <pratikjena488@gmail.com>
2025-04-06 16:40:21 +0530

    third commit - updated BMI logic and layout

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
   return (
     <div className="app">
       <div className='container'>
-        <h2 className='center'>BMI Calculator</h2>
+        <h2 className='center'>BMI CALCULATOR</h2>
         <form onSubmit={calcBmi}>
           <div>
             <label>Weight (lbs)</label>
